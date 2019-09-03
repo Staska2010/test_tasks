d385bacb34bb478e522726de894f446374689869
Stanislav Klokov <klokovstas2013@gmail.com>
2019-09-03 11:38:23 +0300

    Объединить два массива [#124482]

--- src/main/java/ru/job4j/test_tasks/Join.java
+++ src/main/java/ru/job4j/test_tasks/Join.java
@@ -3,17 +3,17 @@ package ru.job4j.test_tasks;
 public class Join {
     public int[] merge(int[] left, int[] right) {
         int[] result = new int[left.length + right.length];
-        System.arraycopy(left, 0, result, 0, left.length);
-        for (int i = 0; i < right.length; i++) {
-            for (int j = 0; j < left.length + i; j++) {
-                if (right[i] <= result[j]) {
-                    System.arraycopy(result, j, result, j + 1, left.length + i - j);
-                    result[j] = right[i];
-                    break;
-                } else if (j == (left.length + i - 1)) {
-                    result[j + 1] = right[i];
-                }
-            }
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while ((i < left.length) && (j < right.length)) {
+            result[index++] = left[i] < right[j] ? left[i++] : right[j++];
+        }
+        while (i < left.length) {
+            result[index++] = left[i++];
+        }
+        while (j < right.length) {
+            result[index++] = right[j++];
         }
         return result;
     }
